e5e00b5cfe78a50b2420a2a75eae67670f619a83
Ezequiel <eze.xray@gmail.com>
2025-04-19 09:24:50 -0300

    TP5-Funciones ejercicio 8

---
+++ 8.py
@@ -0,0 +1,16 @@
+"""
+ 8. Crear una función llamada calcular_imc(peso, altura) que reciba el
+ peso en kilogramos y la altura en metros, y devuelva el índice de
+ masa corporal (IMC). Solicitar al usuario los datos y llamar a la fun
+ción para mostrar el resultado con dos decimales.
+"""
+
+peso = float(input("ingrese su peso (KG):"))
+altura = float(input("ingrese su altura (metros): "))
+
+def calcular_imc(peso, altura):
+    imc = peso / (altura ** 2)
+    return imc
+
+imc = calcular_imc(peso,altura)
+print(f"el indice de masa corporal (IMC) es: {imc:.2f}")
