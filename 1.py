51dba51f7fed7689b52c7c6197086671f5afc5e6
Ezequiel <eze.xray@gmail.com>
2025-04-19 07:53:54 -0300

    TP5-Funciones ejercicio 1

---
+++ 1.py
@@ -0,0 +1,11 @@
+"""
+1. Crear una función llamada imprimir_hola_mundo que imprima por
+ pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el
+ programa principal.
+"""
+#funcion para imprimir "Hola mundo!"
+def imprimir_hola_mundo():
+    return print("Hola Mundo!")
+
+#invocamos la funcion desde el programa principal:
+imprimir_hola_mundo()
