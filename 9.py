38d481d9d71873395c8dec1a2f2e8ee7df6b1642
Ezequiel <eze.xray@gmail.com>
2025-04-19 09:53:27 -0300

    TP5-Funciones ejercicio 9 comments

--- 9.py
+++ 9.py
@@ -4,12 +4,16 @@
  Fahrenheit. Pedir al usuario la temperatura en Celsius y mostrar el
  resultado usando la función.
 """
-
+#solicitamos al usuario grados en celsius
 grados_celsius = float(input("ingrese grados celsius: "))
 
+#se crea la funcion para convertir grados
 def celsius_a_fahrenheit(celsius):
     farenheit = (celsius * 1.8) + 32
     return farenheit
 
+#almacenamos el return de la funcion en la variable conversion
 conversion = celsius_a_fahrenheit(grados_celsius)
+
+#mostramos en pantalla el resultado
 print(f"grados celsius({grados_celsius}°C) a fahrenheit = {conversion} °F")
